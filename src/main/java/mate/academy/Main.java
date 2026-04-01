e32617279d3435e28018e867ef6ed2e87fe2f3a1
Krzysztof Frynas <cz.damax@gmail.com>
2026-04-01 15:30:19 +0200

    changed implementation and added tests to main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,7 @@
 package mate.academy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
@@ -8,39 +10,37 @@ import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 public class Main {
     public static void main(String[] args) {
-        MovieService movieService =
-                (MovieService) Injector.getInstance("mate.academy");
+
         CinemaHallService cinemaHallService =
                 (CinemaHallService) Injector.getInstance("mate.academy");
-        MovieSessionService movieSessionService =
-                (MovieSessionService) Injector.getInstance("mate.academy");
-
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
-
-        Movie movie = movieService.add(new Movie("Batman"));
-
         CinemaHall hall = new CinemaHall();
         hall.setCapacity(100);
         hall.setDescription("Blue hall");
         hall = cinemaHallService.add(hall);
+
+        MovieService movieService =
+                (MovieService) Injector.getInstance("mate.academy");
+        Movie movie = movieService.add(new Movie("Batman"));
 
         MovieSession session = new MovieSession();
         session.setMovie(movie);
         session.setCinemaHall(hall);
         session.setShowTime(LocalDateTime.now());
 
+        MovieSessionService movieSessionService =
+                (MovieSessionService) Injector.getInstance("mate.academy");
+
         movieSessionService.add(session);
 
         movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
                 .forEach(System.out::println);
+
+        Movie fastAndFurious = new Movie("Fast and Furious");
+        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        movieService.add(fastAndFurious);
+        System.out.println(movieService.get(fastAndFurious.getId()));
+        movieService.getAll().forEach(System.out::println);
     }
 }
