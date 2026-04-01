ff3eb7180433606d8b6dc2da2745270dfa70593c
Krzysztof Frynas <cz.damax@gmail.com>
2026-04-01 15:25:35 +0200

    changed implementation and added tests to main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,10 +1,15 @@
 package mate.academy;
 
 import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,5 +25,22 @@ public class Main {
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        Movie movie = movieService.add(new Movie("Batman"));
+
+        CinemaHall hall = new CinemaHall();
+        hall.setCapacity(100);
+        hall.setDescription("Blue hall");
+        hall = cinemaHallService.add(hall);
+
+        MovieSession session = new MovieSession();
+        session.setMovie(movie);
+        session.setCinemaHall(hall);
+        session.setShowTime(LocalDateTime.now());
+
+        movieSessionService.add(session);
+
+        movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
+                .forEach(System.out::println);
     }
 }
